086d3d9519f6a14318e6b93989d7745010d07915
Wasim7x <wasim7x@gmail.com>
2026-04-14 19:28:24 +0530

    config file path resolve

--- ui/uiconfigfile.py
+++ ui/uiconfigfile.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 
 class Config:
-    def __init__(self,config_file=r"ui\\uiconfigfile.ini"):
+    def __init__(self,config_file = "ui/uiconfigfile.ini"):
         self.config=ConfigParser()
         self.config.read(config_file)
 
